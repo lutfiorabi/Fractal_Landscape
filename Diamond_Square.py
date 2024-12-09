6f3b899574a081553ab0be67fe7e15e1e6481e25
Lutfi <lutfi.a.orabi@gmail.com>
2024-12-09 16:26:17 +0100

    refactored the functions for better usability and readabilty and added comments

--- Diamond_Square.py
+++ Diamond_Square.py
@@ -10,12 +10,26 @@ With each iteration the scale is divided by scale_step to insure that closer dat
 '''
 
 
-def diamond_square(size, scale_step, iterations):
+def diamond_square(size: int, scale_step: float, iterations: int) -> np.ndarray:
+    """
+    Applies the Diamond-Square algorithm on a grid to generate heightmap textures.
+
+    This function utilizes the diamond-square algorithm to generate random terrains.
+    It begins with a grid of the specified size and iteratively applies diamond and
+    square steps to generate height variations. The scale for terrain displacement
+    decreases incrementally based on the scale_step parameter over the specified number
+    of iterations.
+
+    :param size: Grid dimension specifying the width and height of the terrain grid.
+    :param scale_step: Factor by which the displacement scale is reduced after each iteration.
+    :param iterations: Number of times the algorithm will iterate over the grid.
+    :return: A 2D numpy array representing the generated terrain heightmap.
+    """
     # initial scale
     scale = 100.0
 
     # Calculate the midpoint of each square and displace it by the current scale
-    def diamond_step(arr, step_size, scale):
+    def diamond_step(arr:np.ndarray, step_size:int, scale:float) -> None:
         half_step = step_size // 2
 
         for x in range(half_step, size, step_size):
@@ -27,7 +41,7 @@ def diamond_square(size, scale_step, iterations):
                 arr[x, y] = avg + (np.random.rand() - 0.5) * scale
 
     # Calculate the midpoint of each edge and displace it by the current scale
-    def square_step(arr, step_size, scale):
+    def square_step(arr:np.ndarray, step_size:int, scale:float) -> None:
         half_step = step_size // 2
 
         for x in range(0, size, half_step):
@@ -60,7 +74,23 @@ def diamond_square(size, scale_step, iterations):
     return arr
 
 
-def show_results(terrain, i=0, last=False, colormap='terrain'):
+def show_results(terrain, i=0, last=False, colormap='terrain', directory="results"):
+    """
+    Displays and saves visualizations of a terrain matrix. Depending on the arguments, this
+    function can generate either an initial or iterative view or a final view of the terrain.
+    For non-final views, it displays a top-down and 3D side view, whereas for the final view,
+    multiple 3D angles are shown.
+
+    :param terrain: 2D array representing the terrain's height values over a grid.
+    :type terrain: np.ndarray
+    :param i: Iteration number used in the title and filename of the saved image.
+    :type i: int
+    :param last: Flag indicating whether this is the final visualization.
+    :type last: bool
+    :param colormap: Name of the matplotlib colormap to use for the visualization.
+    :type colormap: str
+    :return: None
+    """
     x, y = np.meshgrid(np.arange(terrain.shape[0]), np.arange(terrain.shape[1]))
 
     def plot_3D(ax, terrain, angle):
@@ -98,21 +128,13 @@ def show_results(terrain, i=0, last=False, colormap='terrain'):
         plt.savefig(f"{directory}/final_{i}.png")
 
 
-if __name__ == '__main__':
-
+def main() -> None:
     directory = "results"
 
     # Parameters
-    print("Enter the number of iterations: ", end="")
-    while True:
-        ITERATION = int(input())
-        if ITERATION <= 0:
-            print("ITERATION must be greater than zero.\nEnter the number of iterations: ", end="")
-            continue
-        break
-
-
-    SIZE = 2 ** ITERATION + 1  # 2^n+1 x 2^n+1 grid
+    ITERATION: int = 10
+    assert(ITERATION > 0)
+    SIZE: int = 2 ** ITERATION + 1  # 2^n+1 x 2^n+1 grid
     print(f"The Grid size will be {SIZE}x{SIZE}")
     SCALE_STEP = 2.0  # How much is the scale divided by in each iteration?
 
@@ -124,5 +146,7 @@ if __name__ == '__main__':
 
     terrain = diamond_square(SIZE, SCALE_STEP, ITERATION)
 
-    show_results(terrain, last=True, i=0, colormap='PuRd_r')
-    show_results(terrain, last=True, i=1, colormap='terrain')
+    show_results(terrain, last=True, i=1, colormap='terrain', directory=directory)
+
+if __name__ == '__main__':
+    main()
